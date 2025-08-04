94ca2fcabe8f92941edb52542d97297c73a1cf97
aminamjad16 <156099179+aminamjad16@users.noreply.github.com>
2025-08-04 21:26:21 +0800

    Fix typo in switch component import

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * * SwitchPrimitives from "@radix-ui/react-switch"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils"
 
